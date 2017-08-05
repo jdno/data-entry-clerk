bec1ffba172c2b2c0c2bb0ee11b39307b8216106
Jan David <jandavid@automatiqa.rocks>
2017-08-05 10:36:41 +0200

    Create and load environment specific configuration
    To be able to configure the application for different environments,
    e.g. development and production, we configure each environment in
    its own file and load them based on the value of an environment
    variable.

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ The Data Entry Clerk (DEC) accepts data on your behalf and stores it in a
 database for later processing. The data is delivered to the DEC by external
 services via webhooks, for which it implements an HTTP endpoint.
 """
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # Load the default configuration
 app.config.from_object('config.default')
+
+# Load environment specific configuration
+app.config.from_object(
+    f"config.{os.getenv('FLASK_ENV', 'development').lower()}")
 
 
 @app.route("/")
---
+++ config/development.py
@@ -0,0 +1,6 @@
+"""
+Development configuration.
+
+The configuration for local development ensures that the application uses local
+or temporary resources, so that new features can be tested in isolation.
+"""
