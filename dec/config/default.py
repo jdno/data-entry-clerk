29bff9aee5d02c508634d64ab3606971cdc11cf0
Jan David <jandavid@automatiqa.rocks>
2017-08-06 12:35:45 +0200

    Add configuration for DynamoDB
    There are a few settings users might want to customize regarding
    their deployment. For DynamoDB, the interesting settings are the
    region, the endpoint (albeit almost identical with region) and the
    table name. For development, we provide defaults for these values,
    while for production, the user needs to customize these settings.

--- dec/config/default.py
+++ dec/config/default.py
@@ -8,3 +8,12 @@ of environment variables.
 
 # Enable debug mode for Flask
 DEBUG = True
+
+# Leave empty to use default DynamoDB endpoint in current region
+DYNAMODB_ENDPOINT = None
+
+# Leave empty to use default region
+DYNAMODB_REGION = None
+
+# Overwrite this in environment specific configuration files
+DYNAMODB_TABLE = "automatiqa-dec"
